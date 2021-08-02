0e840902e722f11b87582afae777695dd6124efb
Bipin <developer.support@astontech.com>
2021-08-02 10:44:05 -0500

    Initial Commit

---
+++ SwiftUI-BarcodeScanner/SwiftUI_BarcodeScannerApp.swift
@@ -0,0 +1,17 @@
+//
+//  SwiftUI_BarcodeScannerApp.swift
+//  SwiftUI-BarcodeScanner
+//
+//  Created by Aston Developer on 8/2/21.
+//
+
+import SwiftUI
+
+@main
+struct SwiftUI_BarcodeScannerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
